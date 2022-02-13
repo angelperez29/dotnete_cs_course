1c22871eb77f8640b66ad6adfb63b7deda860258
angelperez29 <dev.angel.p@gmail.com>
2022-02-13 13:03:44 -0600

    Continued with .NET Class Library

--- starting/fsteps/ConsoleApp2.cs
+++ starting/fsteps/ConsoleApp2.cs
@@ -6,6 +6,10 @@ namespace ConsoleApp2{
             int first_number = 12;
             int second_number = 7;
             Console.WriteLine(first_number+second_number);
+            Console.WriteLine("\nSesión para prubas de incrementno/decremento\n");
+            int value = 0;
+            value = value +5;
+            Console.WriteLine("Este es el resultado: " + value + " de la forma value = value +5");
         }
     }
 }
